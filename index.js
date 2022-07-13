cf0e75c1ed101cc94874af11b2c064102608df75
Garridopedro <pe.santos.garrido@gmail.com>
2022-07-13 17:11:32 -0300

    criando código para organizar css pelo prompt

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+import prompt from "prompt-sync"
 
+const comando = prompt();
 
+const ordemAlfabetica = () => {
+    let css = comando("Digite um elemento CSS:");
+    let array = [];
+    while (css.toLowerCase() != "sair") {
+        array.push(css);
+        css = comando("Digite um elemento CSS:")
+    }
+    return array.sort().join(" " + "/ ");
+}
+
+console.log(ordemAlfabetica());
